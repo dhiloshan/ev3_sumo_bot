affa8eb30c557b435b9814e632528b26c3ea2af6
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-06-02 11:21:33 -0400

    added turn function

--- maze.py
+++ maze.py
@@ -14,13 +14,31 @@ leftBackM = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 rightFrontM = Motor(Port.C, Direction.CLOCKWISE)
 rightBackM = Motor(Port.D, Direction.COUNTERCLOCKWISE)
 
-def drive(speed: int, reverse: bool, time: int) -> None:
-    if(reverse == true):
+def drive(speed: int, time: int, reverse: bool) -> None:
+    if(reverse == True):
         speed *= -1
+
     for motor in [leftFrontM, leftBackM, rightFrontM, rightBackM]:
         motor.run(speed)
+    wait(time)
+
+    for motor in [leftFrontM, leftBackM, rightFrontM, rightBackM]:
+        motor.stop()
+
+def turn(speed: int, tim`e: int, turnLeft: bool) -> None:
+    # turnLeft = True -> left turn
+    # turnLeft = False -> right turn
+    if(turnLeft == False):
+        speed *= -1
+
+    for leftMotor in [leftFrontM, leftBackM]:
+        leftMotor.run(speed)
+    for rightMotor in [rightFrontM, rightBackM]:
+        rightMotor.run(-speed)
+
     wait(time)
     for motor in [leftFrontM, leftBackM, rightFrontM, rightBackM]:
         motor.stop()
 
-drive(1500, true, 3000)
+# 1050 is the max speed
+turn(1050, 4000, True) # perfect 90 degree turn
