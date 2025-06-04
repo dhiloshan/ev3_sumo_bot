0622fa425e24f6ea56cd3e2e33a649929dd09f7b
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-06-04 11:47:31 -0400

    additional functions for final battle; got robot to run until it is 18 cm away from the wall (removed the wait func)

---
+++ template.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+# ports are reversed
+ev3 = EV3Brick()
+leftFrontM = Motor(Port.D, Direction.CLOCKWISE)
+leftBackM = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+rightFrontM = Motor(Port.B, Direction.CLOCKWISE)
+rightBackM = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+frontUS = UltrasonicSensor(Port.S1)
+frontCS = ColorSensor(Port.S2)
+backCS = ColorSensor(Port.S3)
+# backGyro = GyroSensor(Port.S4)
+
+def drive_time(speed: int, time: int, reverse: bool) -> None:
+    if(reverse == True):
+        speed *= -1
+
+    for motor in [leftFrontM, leftBackM, rightFrontM, rightBackM]:
+        motor.run(speed)
+    wait(time)
+
+    for motor in [leftFrontM, leftBackM, rightFrontM, rightBackM]:
+        motor.stop()
+
+def drive(speed: int, reverse: bool) -> None:
+    if(reverse == True):
+        speed *= -1
+
+    for motor in [leftFrontM, leftBackM, rightFrontM, rightBackM]:
+        motor.run(speed)
+
+def stopMotors() -> None:
+    for motor in [leftFrontM, leftBackM, rightFrontM, rightBackM]:
+        motor.stop()
+
+def turn(speed: int, time: int, turnLeft: bool) -> None:
+    # turnLeft = True -> left turn
+    # turnLeft = False -> right turn
+    if(turnLeft == False):
+        speed *= -1
+
+    for leftMotor in [leftFrontM, leftBackM]:
+        leftMotor.run(speed)
+    for rightMotor in [rightFrontM, rightBackM]:
+        rightMotor.run(-speed)
+
+    wait(time)
+    for motor in [leftFrontM, leftBackM, rightFrontM, rightBackM]:
+        motor.stop()
+"""
+def detectCollision() -> bool:
+    # checks if the other functions are running
+    # if the angle changes (fork going upward), then turn to get away from the robot
+"""
+
+while True:
+    drive(1050, True)
+    frontDist = frontUS.distance()
+    if frontDist < 180:
+        stopMotors()
